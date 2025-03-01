e8a0f026798630cc8076ec6326a4ecffe5b1bd8f
Ebrar Öz <ebrarozx@gmail.com>
2025-03-01 17:41:25 +0300

    first commit

---
+++ h1ping.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'saldırılacak ıp adresini gir veya domain adresini
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C'
+read padres
+ping $padres
